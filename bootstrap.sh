0df8b45c32855d40f374832b62965a289b82d2a6
RazvanCretu <razvan.cretu97@gmail.com>
2024-02-04 21:47:43 +0000

    Update:
    Fixes on bootstraping script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,20 @@ usr=/home/ubuntu
 add-apt-repository ppa:git-core/ppa -y # required for latest github releases
 apt update -y
 apt upgrade -y
-apt install unzip -y
-apt install git -y
+apt install unzip git mysql-server expect -y
+
+systemctl start mysql.service
+
+# Make sure that NOBODY can access the server without a password
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';"
+# Kill the anonymous users
+mysql -e "DROP USER ''@'localhost'"
+# Because our hostname varies we'll use some Bash magic here.
+mysql -e "DROP USER ''@'$(hostname)'"
+# Kill off the demo database
+mysql -e "DROP DATABASE test"
+# Make our changes take effect
+mysql -e "FLUSH PRIVILEGES"
 
 ##### AWS #####
 
@@ -73,7 +85,7 @@ su - ubuntu -c "npm config set prefix $usr/.npm-global"
 
 printf '\n\nexport PATH=~/.npm-global/bin:$PATH' >> $usr/.bashrc
 
-su - ubuntu -c "npm install -g npm@10.2.5 pm2"
+su - ubuntu -c "npm install -g npm@latest pm2"
 
 
 $HOME
